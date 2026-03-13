88c8001411218bfacbb9a21e64780982d67f6cd3
Sylvino Prevot <sylvino.junior@calindra.com.br>
2026-03-13 01:21:33 -0300

    first commit

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (title.trim() && description.trim()) {
+            onAddTaskSubmit(title, description);
+            setTitle("");
+            setDescription("");
+          } else {
+            return alert("Preencha o título e a descrição da tarefa.");
+          }
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-mp font-medium"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+export default AddTask;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,35 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted ? "line-through" : ""}`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => {
+              if (task.isCompleted === false) {
+                return alert("Você precisa terminar a tarefa primeiro.");
+              } else {
+                onDeleteTaskClick(task.id);
+              }
+            }}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <TrashIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
